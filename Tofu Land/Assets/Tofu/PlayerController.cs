8bbfb68f149f1354534311ce5a469ea9cb640246
Joshua Thomas <jthomas2023@chadwickschool.org>
2020-11-05 11:35:48 -0800

    Milestone 1: working on my tofu script
    I created the tofu script and my tofu is now able to walk forward and
    backward. My tofu now has gravity effect as well so it can fall off
    the edge and stand on the tiles

---
+++ Tofu Land/Assets/Tofu/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Determines the speed that the Tofu moves
+    public float speed;
+
+// Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime * horizontalInput);
+
+        if (horizontalInput < 0) 
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        } else
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if (horizontalInput != 0)
+        {
+            GetComponent<Animator>().SetBool("isMoving", true);
+        } else
+        {
+            GetComponent<Animator>().SetBool("isMoving", false);
+        }
+
+    }
+}
