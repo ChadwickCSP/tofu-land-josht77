563c7b91152813454ab4144b7276c13b210e9580
Joshua Thomas <jthomas2023@chadwickschool.org>
2020-11-19 11:29:08 -0800

    milestone 5: commits
    trying to make the advanced feature for milestone 5. Having problems

---
+++ Tofu Land/Assets/Enemy/EnemySpawnerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    public GameObject enemy;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 5f;
+    float nextSpawn = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range (-8f, 8f);
+            whereToSpawn = new Vector3 (randX, transform.position.y, -5);
+            GameObject newEnemy = Instantiate (enemy, whereToSpawn, Quaternion.identity);
+            newEnemy.transform.position = whereToSpawn;
+        }
+    }
+}
